d72d986bae4d6225c40b4ddc29eb7bd3bfcd106d
Max Marre <maxnmarre@gmail.com>
2024-09-15 15:56:46 -0600

    Overrides equals and hashCode in the ChessMove and ChessPosition classes

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -9,6 +11,7 @@ package chess;
 public class ChessPosition {
 
     private final int row;
+
     private final int col;
 
     public ChessPosition(int row, int col) {
@@ -30,5 +33,18 @@ public class ChessPosition {
      */
     public int getColumn() {
         return col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return getRow() == that.getRow() && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRow(), col);
     }
 }
