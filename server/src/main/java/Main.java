34f57b413573aa56b6712ed018382cb1166f14e8
Max Marre <maxnmarre@gmail.com>
2024-10-14 22:28:55 -0600

    init(phase3) Adds starter code for Phase 3 and installs dependencies

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,7 +1,9 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
+        new Server();
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
