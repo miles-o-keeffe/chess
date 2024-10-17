2ca6d5b66bf3b19f805b4655a7d090129cd4b6ff
Max Marre <maxnmarre@gmail.com>
2024-10-16 18:50:11 -0600

    Adds methods to the DataAccess Interface

--- server/src/main/java/dataAccess/DataAccess.java
+++ server/src/main/java/dataAccess/DataAccess.java
@@ -1,7 +1,29 @@
 package dataAccess;
 
+import model.AuthData;
+import model.GameData;
 import model.UserData;
 
+import java.util.Collection;
+
 public interface DataAccess {
+    void clear();
+
+    UserData createUser(UserData newUser);
+
     UserData getUser(String userName);
+
+    int createGame(); // Returns the gameID
+
+    GameData getGame();
+
+    Collection<GameData> listGame();
+
+    void updateGame();
+
+    AuthData createAuth();
+
+    AuthData getAuth();
+
+    void deleteAuth();
 }
--- server/src/main/java/dataAccess/MemoryDataAccess.java
+++ server/src/main/java/dataAccess/MemoryDataAccess.java
@@ -1,14 +1,63 @@
 package dataAccess;
 
+import model.AuthData;
+import model.GameData;
 import model.UserData;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 public class MemoryDataAccess implements DataAccess {
     // final private map<String, UserData> users = new HashMap<>();
 
     @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public UserData createUser(UserData newUser) {
+        return null;
+    }
+
+    @Override
     public UserData getUser(String userName) {
         return null;
+    }
+
+    @Override
+    public int createGame() {
+        return 0;
+    }
+
+    @Override
+    public GameData getGame() {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGame() {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame() {
+
+    }
+
+    @Override
+    public AuthData createAuth() {
+        return null;
+    }
+
+    @Override
+    public AuthData getAuth() {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth() {
+
     }
 }
