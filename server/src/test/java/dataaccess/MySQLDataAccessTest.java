5ca531d4d276b89b135ed5688142474db4f5bae7
Max Marre <maxnmarre@gmail.com>
2024-11-01 18:09:06 -0600

    Implements createGame and getUser tests

--- server/src/test/java/dataaccess/MySQLDataAccessTest.java
+++ server/src/test/java/dataaccess/MySQLDataAccessTest.java
@@ -5,6 +5,9 @@ import exception.ResponseException;
 import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.*;
+import org.mindrot.jbcrypt.BCrypt;
+import request.CreateGameRequest;
+import request.JoinGameRequest;
 import request.RegisterRequest;
 
 public class MySQLDataAccessTest {
@@ -25,7 +28,7 @@ public class MySQLDataAccessTest {
 
     @Test
     void clearTest() throws DataAccessException {
-        // Adds to all the DATA_ACCESS fields so that they can be cleared
+        // Adds data to all the database tables and then clears them
         for (int i = 1; i < 11; i++) {
             UserData testUserData = new UserData(testUsername + (i),
                     testPassword + (i), testEmail + (i));
@@ -40,28 +43,49 @@ public class MySQLDataAccessTest {
     }
 
     @Test
-    void positiveCreateUser() {
+    void positiveCreateUser() throws DataAccessException {
+        UserData testUserData = new UserData(testUsername,
+                testPassword, testEmail);
 
+        dataAccess.createUser(testUserData);
+
+        Assertions.assertEquals(testUserData.username(), dataAccess.getUser(testUsername).username());
+        Assertions.assertTrue(BCrypt.checkpw(testUserData.password(), dataAccess.getUser(testUsername).password()));
+        Assertions.assertEquals(testUserData.email(), dataAccess.getUser(testUsername).email());
     }
 
     @Test
-    void negativeCreateUser() {
+    void negativeCreateUser() throws DataAccessException {
+        UserData testUserData = new UserData(null,
+                null, null);
 
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            dataAccess.createUser(testUserData);
+        });
     }
 
     @Test
-    void positiveGetUser() {
+    void positiveGetUser() throws DataAccessException {
+        positiveCreateUser(); // These tests are the same
 
+        Assertions.assertNull(dataAccess.getUser(null));
+
+        Assertions.assertNull(dataAccess.getUser("non_existent_username"));
     }
 
     @Test
-    void negativeGetUser() {
-
+    void negativeGetUser() throws DataAccessException {
+        // The way my getUser is written there isn't really a negative case
+        Assertions.assertNull(dataAccess.getUser(null));
     }
 
     @Test
-    void positiveCreateGame() {
+    void positiveCreateGame() throws DataAccessException {
+        int gameID = dataAccess.createGame("testGameName1");
+        GameData gameData = new GameData(gameID, null, null,
+                "testGameName1", new ChessGame());
 
+        Assertions.assertEquals(gameData, dataAccess.getGame(gameID));
     }
 
     @Test
